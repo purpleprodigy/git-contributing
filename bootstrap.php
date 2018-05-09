5ce3361b9430f61f03dc7d80cbc80add50190a1a
Rose Cox <rose@purpleprodigy.com>
2018-05-09 16:03:31 +1000

    Further improvement to descriptions

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: Git Contributing
  * Plugin URI:  https://github.com/KnowTheCode/git-contributing
- * Description: An awesome collaborative project to teach you the contributor workflow.
+ * Description: An awesome collaborative project to teach you the contributor workflow for the git contributing hands-on lab.
  * Version:     1.0.0
  * Author:      Git Contributing Team
  * Author URI:  https://KnowTheCode.io
